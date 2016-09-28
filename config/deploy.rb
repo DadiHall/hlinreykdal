f6c8928576f643b5b802b95f117cf323ad05cd6a
DadiHall <dadih32@gmail.com>
2016-09-28 16:25:04 +0000

    rremoved bin from deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,7 @@ set :deploy_to, '/home/deploy/hlinreykdal'
 
 
 set :linked_files, %w{config/database.yml config/secrets.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
 
