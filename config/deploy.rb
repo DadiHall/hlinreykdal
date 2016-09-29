199d164a43225b72c34075fb915a6d484827f80d
DadiHall <dadih32@gmail.com>
2016-09-29 20:59:53 +0000

    added config/application.yml to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :deploy_to, '/home/deploy/hlinreykdal'
 
 
 
-set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml config/application.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
