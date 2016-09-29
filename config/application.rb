9af2ac06189591fd183214de222809d88a99b1b1
DadiHall <dadih32@gmail.com>
2016-09-29 00:52:24 +0000

    Added paperclip code to application.rb

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+Paperclip.options[:command_path] = "/usr/local/bin/identify"
 module Hlinreykdal
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
