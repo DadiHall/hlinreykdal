7e1aba32bb76f0511183764f984062cfa14875f1
DadiHall <dadih32@gmail.com>
2016-09-29 21:46:14 +0000

    added stock quantity to admin and product show

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -8,6 +8,7 @@ permit_params :title, :description, :image, :price_usd, :price_isl, :category_id
 		column :category
 		column :label
 		column :created_at
+		column :stock_quantity
 		column :price_isl, :sortable => :price_isl do |product|
 			number_to_currency product.price_isl
 		end
