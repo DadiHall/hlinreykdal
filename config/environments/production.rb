1a56fce8ca147b21b68b0596e8ce8af1c4567f42
DadiHall <dadih32@gmail.com>
2017-05-22 15:09:51 +0000

    added inspect to env variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: ENV["SMTP_ADDRESS"],
+    address: ENV["SMTP_ADDRESS"].inspect,
     user_name: ENV["SMTP_USER"],
     password: ENV["SMTP_PASSWORD"],
     domain: "hlinreykdal.com",
