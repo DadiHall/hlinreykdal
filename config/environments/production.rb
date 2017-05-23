28a8ea34f5d53b3c72343c501b93ec89acaba725
DadiHall <dadih32@gmail.com>
2017-05-23 23:35:40 +0000

    changed ip to www

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   
-  config.action_mailer.default_url_options = { host: '138.68.150.155'}
+  config.action_mailer.default_url_options = { host: 'www.hlinreykdal.com'}
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
